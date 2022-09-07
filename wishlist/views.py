ee6d60931b8ce1ab06a7e39e718f2ac8fada3711
eugeniusms <eugeniusms@gmail.com>
2022-09-07 19:21:03 +0700

    Tutorial: Menghubungkan MVT Done

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from wishlist.models import BarangWishlist
 
 # Create your views here.
 def show_wishlist(request):
-    return render(request, "wishlist.html")
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Eugenius Mario Situmorang'
+    }
+    return render(request, "wishlist.html", context)
