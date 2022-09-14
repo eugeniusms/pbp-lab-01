06b74d78aa1ce228339b9bd11d70b9d3f7cf4811
eugeniusms <eugeniusms@gmail.com>
2022-09-15 05:43:36 +0700

    Add GET Single Data (XML & JSON)

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from wishlist.views import show_wishlist
 from wishlist.views import show_xml
 from wishlist.views import show_json
+from wishlist.views import show_xml_by_id
+from wishlist.views import show_json_by_id
 
 app_name = 'wishlist'
 
@@ -9,4 +11,6 @@ urlpatterns = [
     path('', show_wishlist, name='show_wishlist'), # http://localhost:8000/wishlist/
     path('xml/', show_xml, name='show_xml'), # http://localhost:8000/wishlist/xml/
     path('json/', show_json, name='show_json'), # http://localhost:8000/wishlist/json/
+    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'), # http://localhost:8000/wishlist/xml/<pk:id>
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), # http://localhost:8000/wishlist/json/<pk:id>
 ]
